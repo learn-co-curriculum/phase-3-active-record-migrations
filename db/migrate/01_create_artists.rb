70bf340d7fc8f62d48633cae783512ff8dc647e7
maxwellbenton <maxwellbenton@gmail.com>
2019-07-16 14:55:56 -0400

    adds solution migrations

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,10 @@
+class CreateArtists < ActiveRecord::Migration[4.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
