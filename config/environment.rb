3c9bf10e106282881c95380e161ec9eacd80e8c3
maxwellbenton <maxwellbenton@gmail.com>
2019-07-16 14:56:07 -0400

    adds db connection solution

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,9 @@ Bundler.require
 
 
 # put the code to connect to the database here
-
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
